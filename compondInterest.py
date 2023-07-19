571a4a1cd338d08aec9b36797e83d8650c9150cb
MuhammedYahiya <muhammedyahiyah@gmail.com>
2023-07-19 15:46:33 +0530

    Python Program for compound interest

--- compondInterest.py
+++ compondInterest.py
@@ -3,6 +3,6 @@ P = float(input("Enter the pricipal amount: "))
 R = float(input("Enter the rate of interest: "))
 T = float(input("Enter the time span: "))
 
-A =float( P*pow(1+R/100,T))
+A =float( P*pow((1+R/100),T))
 CI = float(A - P)
 print("Compound Interest: " + str(CI))
